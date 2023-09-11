4134634fc2ace94333b60a16ac2c5b5257b71fc3
Samalairbien <74256932+SamEPK@users.noreply.github.com>
2023-09-11 22:34:08 +0200

    burger

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loaded");
-    let btn = document.querySelector(".material-icons");
-    let el = document.querySelector("nav");
+    let btn = document.querySelector(".burger");
+    let el = document.querySelector("header div");
+    let down = document.querySelector("nav ul");
+
     btn.addEventListener("click", () => {
+        el.style.transition = "opacity 1s ease-in-out, transform 1 ease-in-out";
         el.classList.toggle("open-menu");
+        btn.innerHTML === `<i class="fa-solid fa-bars"></i>` ? btn.innerHTML = `<i class="fa-solid fa-x"></i>` : btn.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+    });
+
+    down.addEventListener("click", () => {
+        el.style.transition = "opacity 1s ease-in-out, transform 1 ease-in-out";
+        el.classList.toggle("open-menu");
+        btn.innerHTML === `<i class="fa-solid fa-bars"></i>` ? btn.innerHTML = `<i class="fa-solid fa-x"></i>` : btn.innerHTML = `<i class="fa-solid fa-bars"></i>`;
     });
     const userDate = {
         datecopy: new Date(),
