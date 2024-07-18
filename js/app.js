b2ef216668c9c1c3b04dfdb83de9cf6081e0fd84
Samalairbien <74256932+SamEPK@users.noreply.github.com>
2024-07-18 18:54:02 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -5,13 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let down = document.querySelector("nav ul");
 
     btn.addEventListener("click", () => {
-        el.style.transition = "opacity 1s ease-in-out, transform 1 ease-in-out";
         el.classList.toggle("open-menu");
         btn.innerHTML === `<i class="fa-solid fa-bars"></i>` ? btn.innerHTML = `<i class="fa-solid fa-x"></i>` : btn.innerHTML = `<i class="fa-solid fa-bars"></i>`;
     });
 
     down.addEventListener("click", () => {
-        el.style.transition = "opacity 1s ease-in-out, transform 1 ease-in-out";
         el.classList.toggle("open-menu");
         btn.innerHTML === `<i class="fa-solid fa-bars"></i>` ? btn.innerHTML = `<i class="fa-solid fa-x"></i>` : btn.innerHTML = `<i class="fa-solid fa-bars"></i>`;
     });
